b85db2d21fe042e92738e7a67a56d8c08ad10a47
Zayveon8 <zqhill@coastal.edu>
2024-03-04 12:35:20 -0500

    Add js/server.js

---
+++ js/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const contentType = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.png': 'image/png',
+        '.jpg': 'image/jpeg',
+        '.gif': 'image/gif',
+    };
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentTypeHeader = contentType[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500);
+                res.end('Internal Server Error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentTypeHeader });
+            res.end(content, 'utf-8');
+        }
+    });
+
+}).listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
